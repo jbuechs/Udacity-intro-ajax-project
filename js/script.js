7ab777ced28991f8c89714e7ec47099782444ecf
jbuechs <janina.buechner@gmail.com>
2015-07-30 21:54:28 -0400

    fix: update NYT API search URI

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function loadData() {
     var imgUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $body.append('<img class="bgimg" src="' + imgUrl + '">');
 
-    var searchStr = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&api-key=52562f2f953bb8964d949fc0f6263c34:1:67333739';
+    var searchStr = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&api-key=52562f2f953bb8964d949fc0f6263c34%3A1%3A67333739';
     console.log(searchStr);
 
     $.getJSON(URL, function(data){
