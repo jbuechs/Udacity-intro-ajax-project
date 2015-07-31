7ef30860076636b026a90ad617882539c9b7ce45
jbuechs <janina.buechner@gmail.com>
2015-07-30 21:39:24 -0400

    init repository

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+ //   load streetview
+    // YOUR CODE GOES HERE!
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+    $greeting.text("So, you want to live at " + address + "?");
+    var imgUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
+    $body.append('<img class="bgimg" src="' + imgUrl + '">');
+
+    var searchStr = 'http://api.nytimes.com/svc/search/v2/articlesearch.response-format?[q=' + cityStr + ']&api-key=52562f2f953bb8964d949fc0f6263c34:1:67333739';
+    console.log(searchStr);
+
+    $.getJSON(URL, function(data){
+        console.log(data);
+    });
+
+    return false;
+}
+
+$('#form-container').submit(loadData);
