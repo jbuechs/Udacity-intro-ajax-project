3f8b77b4daccd5f685affaf5a98b143c324221fa
jbuechs <janina.buechner@gmail.com>
2015-07-31 10:39:13 -0400

    feat: add list of NYT articles about city - my first AJAX request!

--- js/script.js
+++ js/script.js
@@ -10,8 +10,7 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
- //   load streetview
-    // YOUR CODE GOES HERE!
+    // load streetview image as background
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
@@ -19,14 +18,23 @@ function loadData() {
     var imgUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $body.append('<img class="bgimg" src="' + imgUrl + '">');
 
-    var searchStr = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=madrid&api-key=52562f2f953bb8964d949fc0f6263c34%3A1%3A67333739';
-    console.log(searchStr);
-
+    // load NYT articles, searching for City as keyword
+    var searchStr = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&api-key=52562f2f953bb8964d949fc0f6263c34%3A1%3A67333739';
     $.getJSON(searchStr, function(data){
-        console.log(data);
+        var articleList = data.response.docs;
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        var newArticle;
+        for (var i = 0, len = articleList.length; i < len; i++) {
+            var article = articleList[i];
+            newArticle = '<li class ="article">' + '<a href = "' + article.web_url + '">' + article.headline.main + '</a>' + '</li>' + '<p>' + article.snippet + '</p>';
+            $nytElem.append(newArticle);
+        }
+        console.log(articleList, articleList.length);
     });
 
     return false;
 }
+
+
 
 $('#form-container').submit(loadData);
