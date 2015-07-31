e35924b6f0de2cd4ee1fea58af3692b2ec53701d
jbuechs <janina.buechner@gmail.com>
2015-07-30 21:59:41 -0400

    fix: fix error in url in getJSON function

--- js/script.js
+++ js/script.js
@@ -19,10 +19,10 @@ function loadData() {
     var imgUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $body.append('<img class="bgimg" src="' + imgUrl + '">');
 
-    var searchStr = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&api-key=52562f2f953bb8964d949fc0f6263c34%3A1%3A67333739';
+    var searchStr = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=madrid&api-key=52562f2f953bb8964d949fc0f6263c34%3A1%3A67333739';
     console.log(searchStr);
 
-    $.getJSON(URL, function(data){
+    $.getJSON(searchStr, function(data){
         console.log(data);
     });
 
